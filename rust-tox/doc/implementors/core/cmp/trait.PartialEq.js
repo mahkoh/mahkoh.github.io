9bb218b15df1f18187508e9534a6c389846d7d0c
Julian Orth <ju.orth@gmail.com>
2014-06-13 00:44:44 +0200

    a

---
+++ rust-tox/doc/implementors/core/cmp/trait.PartialEq.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['tox'] = ["impl <a class='trait' href='http://doc.rust-lang.org/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a> for <a class='type' href='tox/core/type.ConnectionStatus.html' title='tox::core::ConnectionStatus'>ConnectionStatus</a>","impl <a class='trait' href='http://doc.rust-lang.org/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a> for <a class='type' href='tox/core/type.UserStatus.html' title='tox::core::UserStatus'>UserStatus</a>","impl <a class='trait' href='http://doc.rust-lang.org/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a> for <a class='type' href='tox/core/type.ChatChange.html' title='tox::core::ChatChange'>ChatChange</a>","impl <a class='trait' href='http://doc.rust-lang.org/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a> for <a class='type' href='tox/core/type.ControlType.html' title='tox::core::ControlType'>ControlType</a>","impl <a class='trait' href='http://doc.rust-lang.org/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a> for <a class='type' href='tox/core/type.Faerr.html' title='tox::core::Faerr'>Faerr</a>","impl <a class='trait' href='http://doc.rust-lang.org/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a> for <a class='type' href='tox/core/type.TransferType.html' title='tox::core::TransferType'>TransferType</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ rust-tox/doc/implementors/core/fmt/trait.Show.js
@@ -0,0 +1,11 @@
+(function() {var implementors = {};
+implementors['rust-tox'] = ["impl <a class='trait' href='http://doc.rust-lang.org/core/fmt/trait.Show.html' title='core::fmt::Show'>Show</a> for <a class='struct' href='rust-tox/core/struct.Address.html' title='rust-tox::core::Address'>Address</a>","impl <a class='trait' href='http://doc.rust-lang.org/core/fmt/trait.Show.html' title='core::fmt::Show'>Show</a> for <a class='struct' href='rust-tox/core/struct.ClientId.html' title='rust-tox::core::ClientId'>ClientId</a>",];
+implementors['tox'] = ["impl <a class='trait' href='http://doc.rust-lang.org/core/fmt/trait.Show.html' title='core::fmt::Show'>Show</a> for <a class='struct' href='tox/core/struct.Address.html' title='tox::core::Address'>Address</a>","impl <a class='trait' href='http://doc.rust-lang.org/core/fmt/trait.Show.html' title='core::fmt::Show'>Show</a> for <a class='struct' href='tox/core/struct.ClientId.html' title='tox::core::ClientId'>ClientId</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
