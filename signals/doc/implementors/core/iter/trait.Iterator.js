dddbc69770cccd68c814d7c8ece678c50efd4262
Julian Orth <ju.orth@gmail.com>
2014-06-26 01:28:37 +0200

    a

---
+++ signals/doc/implementors/core/iter/trait.Iterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['signals'] = ["impl&lt;'a&gt; <a class='trait' href='http://doc.rust-lang.org/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a>&lt;<a class='type' href='signals/type.Signal.html' title='signals::Signal'>Signal</a>&gt; for <a class='struct' href='signals/struct.SignalIter.html' title='signals::SignalIter'>SignalIter</a>&lt;'a&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ signals/doc/implementors/core/ops/trait.Drop.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['signals'] = ["impl <a class='trait' href='http://doc.rust-lang.org/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='signals/struct.Signals.html' title='signals::Signals'>Signals</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
