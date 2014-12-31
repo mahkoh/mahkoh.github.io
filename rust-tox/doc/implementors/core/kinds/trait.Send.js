aa8218bdcf9d1db7e1462a04c99bf805fdc354c8
Julian Orth <ju.orth@gmail.com>
2014-12-31 01:39:10 +0100

    a

---
+++ rust-tox/doc/implementors/core/kinds/trait.Send.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['tox'] = ["<a class='stability Experimental' title='Experimental'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/kinds/trait.Send.html' title='core::kinds::Send'>Send</a> for *mut <a class='struct' href='tox/core/ll/struct.Tox.html' title='tox::core::ll::Tox'>Tox</a>","<a class='stability Experimental' title='Experimental'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/kinds/trait.Send.html' title='core::kinds::Send'>Send</a> for *mut <a class='struct' href='tox/av/ll/struct.ToxAv.html' title='tox::av::ll::ToxAv'>ToxAv</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ rust-tox/doc/implementors/core/num/trait.FromPrimitive.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['tox'] = ["<a class='stability Experimental' title='Experimental: trait is likely to be removed'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/num/trait.FromPrimitive.html' title='core::num::FromPrimitive'>FromPrimitive</a> for <a class='enum' href='tox/av/enum.CallState.html' title='tox::av::CallState'>CallState</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
