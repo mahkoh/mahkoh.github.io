97f689924fd69d5e5d4bb8e018cab0c407891be4
Julian Orth <ju.orth@gmail.com>
2026-02-05 13:39:10 +0100

    update wayland book

--- wayland-book/searcher-c2a407aa.js
+++ wayland-book/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-1cb904f2.js',
+                path_to_root + 'searchindex-c85273cc.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
